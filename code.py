c629f2f467fbbb6de6219ade43d7234c465a15a7
Luis Robles <lroblesr@unmsm.edu.pe>
2021-10-19 01:00:23 -0500

    Join all the images *.jpg in the file output.pdf

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
 from pdf2image import convert_from_path
 from PyPDF2 import PdfFileReader
+import img2pdf
+import os
 
 # File name to convert
 archivo = 'input.pdf'
@@ -8,7 +10,7 @@ archivo = 'input.pdf'
 pdf = PdfFileReader(open(archivo, 'rb'))
 nPages = pdf.getNumPages()
 
-print('Total number of pages: ' + str(nPages))
+print('Total number of pages to convert: ' + str(nPages))
 
 def main():
   # convert PDF pages to JPG
@@ -17,6 +19,14 @@ def main():
     pages = convert_from_path(archivo, first_page=ii, single_file=True)
     page_name = 'page_' + str(ii) + '.jpg'
     pages[0].save(page_name, quality=100)
+
+  # List of all files *.jpg
+  images = [i for i in os.listdir(os.getcwd()) if i.endswith('.jpg')]
+  # print(images)
+
+  # Rewrite all the images in the file output.pdf
+  with open("output.pdf", "wb") as f:
+    f.write(img2pdf.convert(images))
 
 if __name__ == "__main__":
   main()
