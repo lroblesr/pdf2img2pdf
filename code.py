64bf212298b694ea6bde258080959a6b1a4df7f8
Luis Robles <lroblesr@unmsm.edu.pe>
2021-10-19 18:02:47 -0500

    Resolve the order error for the numbering of the images and after the conversion, delete the images

--- code.py
+++ code.py
@@ -1,10 +1,20 @@
 from pdf2image import convert_from_path
 from PyPDF2 import PdfFileReader
 import img2pdf
+import glob
 import os
 
+# Change the current working directory
+# Indicate path where the PDF file is located.
+# Use / instead of \
+os.chdir('C:/Users/rral/Desktop/PP')
+
+# name = 'Protocolo FE_FA'
+name = 'Protocolo de Interpretacion Visual_FA'
+
 # File name to convert
-archivo = 'input.pdf'
+archivo = name + '.pdf'
+print('file to convert: ' + archivo)
 
 # Extract the number of pages that has input.pdf file
 pdf = PdfFileReader(open(archivo, 'rb'))
@@ -17,16 +27,24 @@ def main():
   for i in range(nPages):
     ii = i + 1
     pages = convert_from_path(archivo, first_page=ii, single_file=True)
+
+    # Allows the continuous numbering of the images (01 ... 09)
+    if ii < 10:
+      ii = '0' + str(ii)
+
     page_name = 'page_' + str(ii) + '.jpg'
     pages[0].save(page_name, quality=100)
 
   # List of all files *.jpg
-  images = [i for i in os.listdir(os.getcwd()) if i.endswith('.jpg')]
-  # print(images)
+  images = [j for j in os.listdir(os.getcwd()) if j.endswith('.jpg')]
 
   # Rewrite all the images in the file output.pdf
-  with open("output.pdf", "wb") as f:
-    f.write(img2pdf.convert(images))
+  with open(name + "_444.pdf", "wb") as k:
+    k.write(img2pdf.convert(images))
+
+  # Clean the created images
+  for l in glob.glob("Page_*.jpg"):
+    os.remove(l)
 
 if __name__ == "__main__":
   main()
